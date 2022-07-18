a3797483e0e6b92ed5952f0128e272d8127be504
linpz <linpz@mail.ustc.edu.cn>
2022-07-18 23:32:46 +0800

    1. add Comm_Keys_3

---
+++ include/Comm/example/Communicate_Set.h
@@ -0,0 +1,21 @@
+// ===================
+//  Author: Peize Lin
+//  date: 2022.06.22
+// ===================
+
+#pragma once
+
+#include <set>
+
+namespace Communicate_Set
+{
+	template<typename Tkey>
+	void traverse_keys(
+		const std::set<Tkey> &keys,
+		std::function<void(const Tkey&)> &func)
+	{
+		for(const Tkey &key : keys)
+			func(key);
+	}
+
+}
---
+++ include/Comm/example/Communicate_Vector.h
@@ -0,0 +1,20 @@
+// ===================
+//  Author: Peize Lin
+//  date: 2022.06.22
+// ===================
+
+#pragma once
+
+#include <vector>
+
+namespace Communicate_Vector
+{
+	template<typename Tkey>
+	void traverse_keys(
+		const std::vector<Tkey> &keys,
+		std::function<void(const Tkey&)> &func)
+	{
+		for(const Tkey &key : keys)
+			func(key);
+	}
+}
